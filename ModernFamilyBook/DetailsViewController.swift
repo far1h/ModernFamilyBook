8999f24b2be2e063ea4d5b6033b8154536562f17
far1h <farihmuhammad45@gmail.com>
2024-08-26 16:59:00 +0700

    Add UI Elements

---
+++ ModernFamilyBook/DetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailsViewController.swift
+//  ModernFamilyBook
+//
+//  Created by Farih Muhammad on 26/08/2024.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
